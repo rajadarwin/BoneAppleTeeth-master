3673dcea3f9fa225166fba594a615ae7c00f1b67
10thjeh <glock.evans8@gmail.com>
2021-11-26 23:08:06 +0700

    implemented resep API call

--- app/src/main/java/com/app/boneappleteeth/ResepRepository.java
+++ app/src/main/java/com/app/boneappleteeth/ResepRepository.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ResepRepository {
     static String BASE_URL = "https://bap-api.crusty-crud.xyz/";
 
-    static final ResepServices create(){
+    public static final ResepServices create(){
         Retrofit retrofit = new Retrofit.Builder()
                                 .baseUrl(BASE_URL)
                                 .addConverterFactory(GsonConverterFactory.create())
--- app/src/main/java/com/app/boneappleteeth/ui/search/SearchFragment.java
+++ app/src/main/java/com/app/boneappleteeth/ui/search/SearchFragment.java
@@ -2,6 +2,7 @@ package com.app.boneappleteeth.ui.search;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.app.boneappleteeth.BahanModel;
 import com.app.boneappleteeth.R;
+import com.app.boneappleteeth.ResepModel;
+import com.app.boneappleteeth.ResepRepository;
+import com.app.boneappleteeth.ResepServices;
 import com.app.boneappleteeth.TextMultipleColor;
 import com.app.boneappleteeth.databinding.FragmentSearchBinding;
 import com.app.boneappleteeth.ui.search.adapter.SearchAdapter;
@@ -20,12 +25,17 @@ import com.app.boneappleteeth.ui.search.model.Search;
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SearchFragment extends Fragment {
 
     private SearchViewModel dashboardViewModel;
     private FragmentSearchBinding binding;
     private SearchAdapter adapter;
     private List<Search> searchList = new ArrayList<>();
+    private List<ResepModel> list_of_resep = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -37,7 +47,20 @@ public class SearchFragment extends Fragment {
         String text1 = TextMultipleColor.getColoredSpanned("masak", "#4E9F3D" );
         String text2 = TextMultipleColor.getColoredSpanned("hari ini?", "#000000");
 
+        resepApiCall();
+        for(ResepModel resep : list_of_resep){
+            Log.d("LOOP", "LOOP!!");
+            Search search = new Search();
+            search.setId(resep.getId_menu());
+            search.setTitle(resep.getNama());
+            search.setMenit(resep.getWaktu() + " Menit");
+            search.setImagePath(R.drawable.image_sayur);
+            search.setFavorite(false);
+            searchList.add(search);
+        }
+
         binding.subTitle.setText(Html.fromHtml(text1 + " " + text2));
+
         initAdapter();
         loadData();
         return binding.getRoot();
@@ -56,7 +79,9 @@ public class SearchFragment extends Fragment {
     }
 
     private void loadData() {
-        Search search = new Search();
+        Log.d("List of search", String.valueOf(searchList));
+
+        /*Search search = new Search();
         search.setId(0);
         search.setTitle("Lamb Chop");
         search.setMenit("10 Menit");
@@ -82,8 +107,32 @@ public class SearchFragment extends Fragment {
         search.setTitle("Tes test");
         search.setMenit("10 Menit");
         search.setImagePath(R.drawable.image_sehat);
-        searchList.add(search);
-
+        searchList.add(search);*/
         adapter.notifyDataSetChanged();
+    }
+
+    void resepApiCall(){
+        ResepServices resepServices = ResepRepository.create();
+        resepServices.getResep().enqueue(new Callback<List<ResepModel>>() {
+            @Override
+            public void onResponse(Call<List<ResepModel>> call, Response<List<ResepModel>> response) {
+                for(ResepModel resep : response.body()) {
+                    Log.d("Add resep", resep.getNama());
+                    Search search = new Search();
+                    search.setId(resep.getId_menu());
+                    search.setTitle(resep.getNama());
+                    search.setMenit(resep.getWaktu() + " Menit");
+                    search.setImagePath(R.drawable.image_sayur);
+                    search.setFavorite(false);
+                    searchList.add(search);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<ResepModel>> call, Throwable t) {
+                Log.e("Error", "Error:", t);
+            }
+        });
     }
 }
